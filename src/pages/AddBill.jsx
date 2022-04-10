5c3d623954977229d531761fcc069426ca92af03
boomshakar <hefehmzy@gmail.com>
2022-04-11 00:25:22 +0100

    Add bill page completed

--- src/pages/AddBill.jsx
+++ src/pages/AddBill.jsx
@@ -13,105 +13,62 @@ const { Option } = Select;
 const { TextArea } = Input;
 
 const AddBillTabe = () => {
-	// const data = [
-	// 	{
-	// 		key: '1',
-	// 		name: 'John Brown',
-	// 		age: 32,
-	// 		address: 'New York No. 1 Lake Park',
-	// 		tags: ['nice', 'developer'],
-	// 	},
-	// ];
-	const columns = [
-		{
-			title: "Item",
-			dataIndex: "item",
-			key: "item",
-			render: (text) => <a>{text}</a>,
-		},
-		{
-			title: "Expense Category",
-			dataIndex: "expenseCategory",
-			key: "expenseCategory",
-		},
-		{
-			title: "Description",
-			dataIndex: "description",
-			key: "description",
-		},
-		{
-			title: "Qty",
-			key: "qty",
-			dataIndex: "qty",
-			render: (text) => <a>{text}</a>,
-		},
-		{
-			title: "Price",
-			key: "price",
-			dataIndex: "price",
-			render: (text) => <a>{text}</a>,
-		},
-		{
-			title: "Tax",
-			key: "tax",
-			dataIndex: "tax",
-			render: (text) => <a>{text}</a>,
-		},
-		{
-			title: "Amount",
-			key: "amount",
-			dataIndex: "amount",
-			render: (text) => <a>{text}</a>,
-		},
-	];
-
 	return (
 		<>
-			{/* <Table columns={columns} dataSource={[]} /> */}
-			<table className="table w-full table-fixed border-collapse shadow-lg bg-white">
+			<table className="table w-full table-fixed border rounded-2xl bg-white mt-4">
 				<thead>
 					<tr>
-						<th className="bg-blue-100 border text-left px-3 py-2">Item</th>
-						<th>Expense Category</th>
-						<th>Description</th>
-						<th>Qty</th>
-						<th>Price</th>
-						<th>Tax</th>
-						<th>Amount</th>
+						<th className="border-b-2 text-left mx-2 px-3 py-2">Item</th>
+						<th className="border-b-2">Expense Category</th>
+						<th className="border-b-2">Description</th>
+						<th className="border-b-2" style={{ width: 90 }}>
+							Qty
+						</th>
+						<th className="border-b-2" style={{ width: 90 }}>
+							Price
+						</th>
+						<th className="border-b-2" style={{ width: 120 }}>
+							Tax
+						</th>
+						<th className="border-b-2">Amount</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td className="border px-3 py-2">
+						<td className="border-b px-3 py-4">
 							<Select className=" w-full" placeholder="Choose" onChange={() => {}}>
 								<Option value="Garri">Garri</Option>
 								<Option value="Semo">Semo</Option>
 								<Option value="Amala">Amala</Option>
 							</Select>
 						</td>
-						<td>
+						<td className="border-b px-3 py-4">
 							<Select className=" w-full" placeholder="Choose" onChange={() => {}}>
 								<Option value="Garri">Garri</Option>
 								<Option value="Semo">Semo</Option>
 								<Option value="Amala">Amala</Option>
 							</Select>
 						</td>
-						<td>
+						<td className="border-b px-3 py-4">
 							<Input placeholder="" />
 						</td>
-						<td>
-							<Input type="number" placeholder="" />
+						<td className="border-b px-3 py-4">
+							<div className="flex mx-auto w-3/4">
+								<Input type="number" placeholder="" style={{ width: 50 }} />
+							</div>
 						</td>
-						<td>
-							<Input type="number" placeholder="" />
+						<td className="border-b px-3 py-4">
+							<div className="flex mx-auto w-3/4">
+								<Input type="number" placeholder="" style={{ width: 50 }} />
+							</div>
 						</td>
-						<td>
-							<div className="flex items-center">
-								<Input placeholder="" />
+						<td className="border-b px-3 py-4">
+							<div className="flex items-center justify-between w-5/6 mx-auto">
+								<Input placeholder="" style={{ width: 50 }} />
 								<img src={EditIcn} alt="edit" className="inline w-5  h-5 " />
 							</div>
 						</td>
-						<td>
+						<td className="border-b px-3 py-4">
 							<div className="flex items-center justify-between w-3/4 mx-auto">
 								<span>N0.00</span>
 								<img src={TrashcanIcn} alt="trash" className="inline w-5  h-5 " />
@@ -125,14 +82,34 @@ const AddBillTabe = () => {
 						<td></td>
 						<td></td>
 						<td colSpan="2">
-							<div className="flex flex-col">
-								<div className="flex w-2/4 mx-auto justify-between">
-									<span>Subtotal:</span>
-									<span>N0.00</span>
+							<div className="flex flex-col py-2">
+								<div className="flex w-2/4 mx-auto justify-between my-1">
+									<div className="flex w-full justify-end">
+										<TextContent
+											title="Subtotal:"
+											fontFamily="roboto"
+											fontWeight="500"
+											fontSize="13px"
+											className="flex justify-end"
+										/>
+									</div>
+									<div className="flex w-full justify-end">
+										<TextContent title="N0.00" fontFamily="roboto" fontWeight="500" fontSize="13px" />
+									</div>
 								</div>
-								<div className="flex  w-2/4 mx-auto justify-between">
-									<span>Total (NGN):</span>
-									<span>N0.00</span>
+								<div className="flex w-2/4 mx-auto justify-between my-1">
+									<div className="flex w-full justify-end">
+										<TextContent
+											title="Total (NGN):"
+											fontFamily="roboto"
+											fontWeight="500"
+											fontSize="13px"
+											className="flex justify-end"
+										/>
+									</div>
+									<div className="flex w-full justify-end">
+										<TextContent title="N0.00" fontFamily="roboto" fontWeight="500" fontSize="13px" />
+									</div>
 								</div>
 							</div>
 						</td>
@@ -143,53 +120,13 @@ const AddBillTabe = () => {
 	);
 };
 export const AddBill = () => {
-	const dateFormat = "YYYY-MM-DD";
-	const ITEM_HEIGHT = 48;
-	const ITEM_PADDING_TOP = 8;
-	const MenuProps = {
-		PaperProps: {
-			style: {
-				maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-				width: 250,
-			},
-		},
-	};
-
 	return (
 		<div>
 			<div>
-				<TextContent title="AddBill" block />
-				{/* <FormControl sx={{ m: 1, minWidth: 120 }}>
-					<div className="flex">
-						<div>
-							<MSelect
-								value=""
-								// sx={{
-								// 	width: 120,
-								// 	height: 30,
-								// }}
-								// MenuProps={MenuProps}
-								onChange={() => {}}
-								displayEmpty
-								inputProps={{ "aria-label": "Without label" }}
-							>
-								<MenuItem value="">
-									<em>Placeholder</em>
-								</MenuItem>
-								<MenuItem value={10}>Ten</MenuItem>
-								<MenuItem value={20}>Twenty</MenuItem>
-								<MenuItem value={30}>Thirty</MenuItem>
-							</MSelect>
-						</div>
-					</div>
-				</FormControl> */}
-				<div className="flex">
-					{/* <Select defaultValue={provinceData[0]} style={{ width: 120 }} onChange={()=>{}}>
-        
-          <Option key={province}>{province}</Option>
-      </Select> */}
+				<TextContent title="Add bill" fontSize="26px" fontWeight="700" fontFamily="Inter" block />
+				<div className="flex my-2">
 					<div className="w-1/3">
-						<TextContent title="Vendor" block />
+						<TextContent title="Vendor" fontFamily="Inter" fontWeight="500" block />
 						<Select style={{ width: 200 }} placeholder="Choose" onChange={() => {}}>
 							<Option value="Garri">Garri</Option>
 							<Option value="Semo">Semo</Option>
@@ -199,15 +136,6 @@ export const AddBill = () => {
 					<div className="w-1/3">
 						<TextContent title="Date" block />
 						<DatePicker placeholder="2015-06-06" style={{ width: 200 }} />
-						{/* <TextArea
-							// value={value}
-							// onChange={this.onChange}
-							// placeholder="Controlled autosize"
-							autoSize={{
-								// minRows: 3,
-								maxRows: 5,
-							}}
-						/> */}
 					</div>
 					<div className="w-1/3">
 						<TextContent title="Bill #" block />
@@ -224,26 +152,8 @@ export const AddBill = () => {
 					</div>
 				</div>
 				<div className="flex">
-					{/* <div>
-						<TextContent title="Upload Copy of Bill" block />
-						<Button
-							variant="outlined"
-							size="small"
-							component="label"
-							sx={{
-								// color: 'success.dark',
-								width: 200,
-								display: "flex",
-								justifyContent: "space-between",
-							}}
-							endIcon={<FileUploadOutlined />}
-						>
-							Upload File
-							<input type="file" hidden />
-						</Button>
-					</div> */}
 					<div className="flex flex-col w-2/3">
-						<div className="flex">
+						<div className="flex my-2">
 							<div className="w-1/2">
 								<TextContent title="Currency" block />
 								<Select style={{ width: 200 }} placeholder="USD - US Dolar" onChange={() => {}}>
@@ -257,12 +167,13 @@ export const AddBill = () => {
 								<DatePicker placeholder="2015-06-06" style={{ width: 200 }} />
 							</div>
 						</div>
-						<div className="flex">
+						<div className="flex my-2">
 							<div className="w-1/2">
 								<TextContent title="Upload Copy of Bill" block />
 								<Upload>
 									<Button className="flex items-center justify-between" style={{ width: 200 }}>
-										<span className="text-write-text3">Upload File</span> <UploadOutlined />
+										<span className="text-write-text3">Upload File</span>{" "}
+										<UploadOutlined className="text-write-text3" />
 									</Button>
 								</Upload>
 							</div>
@@ -281,7 +192,7 @@ export const AddBill = () => {
 							</div>
 						</div>
 					</div>
-					<div className="w-1/3">
+					<div className="w-1/3 my-2">
 						<TextContent title="P.O / S.O" block />
 						<TextArea
 							// value={value}
@@ -297,8 +208,12 @@ export const AddBill = () => {
 				</div>
 				<AddBillTabe />
 				<div className="flex justify-end m-4">
-					<button className="border rounded-md py-2 px-4 text-write-text3 m-1">Cancel</button>
-					<button className="border rounded-md py-2 px-4 bg-light-blue text-write-white m-1">Save</button>
+					<button onClink={() => {}} className="border border-write-text1 rounded-md py-3 px-6 text-write-text1 m-1">
+						Cancel
+					</button>
+					<button type="submit" className="border rounded-md py-3 px-6 bg-light-blue text-write-white m-1">
+						Save
+					</button>
 				</div>
 			</div>
 		</div>
